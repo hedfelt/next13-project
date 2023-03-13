0e6fb5cb0d77e79016bb96d0c3b2588498c4813e
Hanne Edfelt <hanne.edfelt@noaignite.com>
2023-03-13 14:22:36 +0100

    feat: removed pages setup and added app directory for next 13 setup

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  // React's Strict Mode is a development mode only feature for highlighting potential problems in an application. It helps to identify unsafe lifecycles, legacy API usage, and a number of other features.
+  reactStrictMode: true,
+  // The app directory is still an experimental feature, so we need to set a flag in the next.config.js file in order to use it:
+  experimental: { appDir: true },
 };
-
 module.exports = nextConfig;
